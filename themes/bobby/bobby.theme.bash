4b0a2ef9ac900718bb008ac2b5b18ba3ed2b97a7
Eric Anders <eric.anders@workiva.com>
2015-10-22 13:37:40 -0500

    Update bobby theme with virtualenv info

--- themes/bobby/bobby.theme.bash
+++ themes/bobby/bobby.theme.bash
@@ -12,9 +12,18 @@ GIT_THEME_PROMPT_SUFFIX="${green}|"
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
+function virtualenv_prompt() {
+    if [ ! -z "$VIRTUAL_ENV" ]
+    then
+        local env=$(dirname $VIRTUAL_ENV)
+        echo "(${env##*/}) "
+    fi
+}
+
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="\n$(battery_char) $(clock_char) ${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
+    PS1="\n$(battery_char) $(clock_char) ${purple}\h ${reset_color}in ${green}\w"
+    PS1="$PS1\n${yellow}$(virtualenv_prompt)${bold_cyan}$(scm_char)${green}$(scm_prompt_info) ${green}→${reset_color} "
 }
 
 PROMPT_COMMAND=prompt_command;
